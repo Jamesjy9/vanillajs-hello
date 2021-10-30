0177fe520e076d738db0d62367696300e563e6f1
Jamesjy9 <jamesnore1989@gmail.com>
2021-10-30 02:18:19 +0000

    random card

--- src/app.js
+++ src/app.js
@@ -7,5 +7,52 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let misc = ["♠", "♣", "♥", "♦"];
+  let cardNumber = [
+    "A",
+    "K",
+    "Q",
+    "J",
+    "10",
+    "9",
+    "8",
+    "7",
+    "6",
+    "5",
+    "4",
+    "3",
+    "2"
+  ];
+  const top = document.querySelector("#top");
+  const bottom = document.querySelector("#bottom");
+  const number = document.querySelector("#number");
+  const card = document.querySelector(".card");
+
+  number.innerHTML = cardNumber[numberAleatorio(cardNumber)];
+
+  let miscAleatorio = misc[numberAleatorio(misc)];
+
+  if (miscAleatorio === "♣" || miscAleatorio === "♠") {
+    if (card.classList.contains("red")) {
+      card.classList.remove("red");
+    } else {
+      card.classList.add("black");
+      top.innerHTML = miscAleatorio;
+      bottom.innerHTML = top.innerHTML;
+    }
+  } else if (miscAleatorio === "♥" || miscAleatorio === "♦") {
+    if (card.classList.contains("black")) {
+      card.classList.remove("black");
+    } else {
+      card.classList.add("red");
+      top.innerHTML = miscAleatorio;
+      bottom.innerHTML = top.innerHTML;
+    }
+  }
 };
+
+function numberAleatorio(arr) {
+  let number = Math.floor(Math.random() * arr.length);
+
+  return number;
+}
